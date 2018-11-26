db018dc5108e9db60417d71a9913c81eae55f6ef
XDS <554536080@qq.com>
2018-11-26 18:54:48 +0800

    11.26

--- app/src/main/java/com/example/xds/mapphotoshare/MainActivity.java
+++ app/src/main/java/com/example/xds/mapphotoshare/MainActivity.java
@@ -1,13 +1,103 @@
 package com.example.xds.mapphotoshare;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textForPosition;
+    LocationClient mLocationClient;
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListener());
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        textForPosition = findViewById(R.id.text_for_position);
+        mapView = findViewById(R.id.baidu_map_view);
+        if (requestPermissions()) {
+            requestLocation();
+        }
+    }
+
+    void requestLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setScanSpan(1000);
+        mLocationClient.setLocOption(option);
+        mLocationClient.start();
+    }
+
+    boolean requestPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.READ_PHONE_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            return false;
+        } else return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (!(grantResults[0] == PackageManager.PERMISSION_GRANTED) ||
+                !(grantResults[1] == PackageManager.PERMISSION_GRANTED) ||
+                !(grantResults[2] == PackageManager.PERMISSION_GRANTED))
+            finish();
+        else {
+            requestLocation();
+        }
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocationClient.stop();
+        mapView.onDestroy();
+    }
+
+    class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            StringBuilder location = new StringBuilder();
+            location.append("经度：").append(bdLocation.getLatitude()).append("\n");
+            location.append("纬度：").append(bdLocation.getLongitude()).append("\n");
+            if (bdLocation.getLocType() == BDLocation.TypeGpsLocation) {
+                location.append("GPS定位");
+            } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkLocation) {
+                location.append("非GPS定位");
+            }
+            textForPosition.setText(location);
+        }
     }
 }
